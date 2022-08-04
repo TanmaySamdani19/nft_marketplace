2dba1f052807fdc2fe1f5716ea5bbf6e34e799e6
Tanmay Samdani <tanmaysamdani12345@gmail.com>
2022-08-04 11:44:44 +0530

    second commit

---
+++ context/NFTContext.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect } from "react";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+import axios from "axios";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+
+import { MarketAddress, MarketAddressABI } from "./constants";
+
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+
+const fetchContracts = (signerOrProvider) =>
+  new ethers.Contract(MarketAddress, MarketAddressABI, signerOrProvider);
+
+export const NFTContext = React.createContext();
+
+export const NFTProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const nftCurrency = "MATIC";
+
+  const checkIfWalletConnected = async () => {
+    if (!window.ethereum) return alert("Please install Metamask");
+
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length) {
+      setCurrentAccount(accounts[0]);
+    } else {
+      console.log("No accounts found.");
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletConnected();
+    createSale("text", "0.025");
+  }, []);
+
+  const connectWallet = async () => {
+    if (!window.ethereum) return alert("Please install Metamask");
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    setCurrentAccount(accounts[0]);
+
+    window.location.reload();
+  };
+
+  const uploadToIPFS = async (file) => {
+    try {
+      const added = await client.add({ content: file });
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+      return url;
+    } catch (error) {
+      console.log("Error uploading file to IPFS.");
+    }
+  };
+
+  const createNFT = async (formInput, fileUrl, router) => {
+    const { name, description, price } = formInput;
+
+    if (!name || !description || !price || !fileUrl) return;
+
+    const data = JSON.stringify({ name, description, image: fileUrl });
+
+    try {
+      const added = await client.add(data);
+
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+      await createSale(url, price);
+
+      router.push("/");
+    } catch (error) {
+      console.log("Error uploading file to IPFS.");
+    }
+  };
+
+  const createSale = async (url, formInputPrice, isReselling, id) => {
+    const web3modal = new Web3Modal();
+    const connection = await web3modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const price = ethers.utils.parseUnits(formInputPrice, "ether");
+    const contract = fetchContracts(signer);
+    const listingPrice = await contract.getListingPrice();
+
+    const transaction = await contract.createToken(url, price, {
+      value: listingPrice.toString(),
+    });
+
+    await transaction.wait();
+  };
+
+  const fetchNFTs = async () => {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const contract = fetchContracts(provider);
+
+    const data = await contract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
+        const tokenURI = await contract.tokenURI(tokenId);
+
+        const {
+          data: { image, name, description },
+        } = await axios.get(tokenURI);
+        const price = ethers.utils.formatUnits(
+          unformattedPrice.toString(),
+          "ether"
+        );
+
+        return {
+          price,
+          tokenId: tokenId.toNumber(),
+          id: tokenId.toNumber(),
+          seller,
+          owner,
+          image,
+          name,
+          description,
+          tokenURI,
+        };
+      })
+    );
+    console.log(items);
+    return items;
+  };
+
+  const fetchMyNFTsOrListedNFTs = async (type) => {
+    const web3modal = new Web3Modal();
+    const connection = await web3modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const contract = fetchContracts(signer);
+
+    const data =
+      type === "fetchItemsListed"
+        ? await contract.fetchItemsListed()
+        : await contract.fetchMyNFTs();
+
+    const items = await Promise.all(
+      data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
+        const tokenURI = await contract.tokenURI(tokenId);
+        console.log(tokenURI);
+        console.log(4);
+        const {
+          data: { image, name, description },
+        } = await axios.get(tokenURI);
+        console.log(5);
+        const price = ethers.utils.formatUnits(
+          unformattedPrice.toString(),
+          "ether"
+        );
+        console.log(6);
+        console.log(
+          price,
+          tokenId.toNumber(),
+          seller,
+          owner,
+          image,
+          name,
+          description,
+          tokenURI
+        );
+        console.log(7);
+        return {
+          price,
+          tokenId: tokenId.toNumber(),
+          seller,
+          owner,
+          image,
+          name,
+          description,
+          tokenURI,
+        };
+      })
+    );
+    console.log(8);
+    console.log(items);
+    return items;
+  };
+
+  return (
+    <NFTContext.Provider
+      value={{
+        nftCurrency,
+        connectWallet,
+        currentAccount,
+        uploadToIPFS,
+        createNFT,
+        fetchNFTs,
+        fetchMyNFTsOrListedNFTs,
+      }}
+    >
+      {children}
+    </NFTContext.Provider>
+  );
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Script from "next/script";
+import { ThemeProvider } from "next-themes";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { NFTProvider } from "../context/NFTContext";
+import { Navbar, Footer } from "../components";
+import "../styles/globals.css";
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
+      <Script
+        src="https://kit.fontawesome.com/8aceaf8f1d.js"
+        crossOrigin="anonymous"
+      />
+    </ThemeProvider>
+  </NFTProvider>
+);
+
+export default MyApp;
